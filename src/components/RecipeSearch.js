42ec76c0e373d73572249723c270bf743b3f3a7f
umairx97 <umair.bajwa@hotmail.com>
2019-01-31 02:50:15 +0500

    Added Search Bar #14

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -4,7 +4,41 @@ export default class RecipeSearch extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello from Recipe Search</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+              <h1 className="text-slanted text-capitalize">
+                Search For Recipes with{" "}
+                <strong className="text-danger">Food2Fork</strong>
+              </h1>
+
+              <form className="mt-4">
+                <label htmlFor="search" className="text-capitalize">
+                  type recipes seperated by comma
+                </label>
+
+                <div className="input-group">
+                  <input
+                    className="input-group"
+                    type="text"
+                    name="search"
+                    placeholder="Chicken, Onions, Carrots"
+                    className="form-control"
+                  />
+
+                  <div className="input-group-append">
+                    <button
+                      type="submit"
+                      className="input-group-text bg-primary text-white"
+                    >
+                      <i className="fas fa-search" />
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
