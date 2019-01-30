b347748c8f9ae27ce658418ec599f1ce0e056e17
umairx97 <umair.bajwa@hotmail.com>
2019-01-30 23:53:12 +0500

    Commit

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Recipie extends Component {
+export default class Recipe extends Component {
   render() {
     const {
       image_url,
